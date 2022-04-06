52ece0895af5018081b1e6ba2adee12470687d37
Sam Colson <samcolson4@gmail.com>
2022-04-06 21:31:47 +0100

    Add sift page

---
+++ app/controllers/sift_controller.rb
@@ -0,0 +1,7 @@
+class SiftController < ApplicationController
+
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "submission#index"
+  get '/sift', to: 'sift#index'
+
 end
