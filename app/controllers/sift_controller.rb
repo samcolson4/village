673137121b4107f8dfd09c73c88112752a586f55
Sam Colson <samcolson4@gmail.com>
2022-04-13 21:42:41 +0100

    Update sift views

--- app/controllers/sift_controller.rb
+++ app/controllers/sift_controller.rb
@@ -1,7 +1,12 @@
 class SiftController < ApplicationController
+  SIFTS = 2
 
   def index
+    @all_sifts = Submission.where(used: false).where(today: false).to_a
 
+    @sifts = []
+    @sifts.append(@all_sifts[0])
+    @sifts.append(@all_sifts[1])
   end
 
 end
